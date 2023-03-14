b41e3883cde1df846e1fc5a5dbcd853392e6c866
Thulani Mtetwa <Thulani.Mtetwa@standardbank.co.za>
2023-03-14 13:10:50 +0200

    Initial Commit

---
+++ CircularProgressbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircularProgressbar
+//
+//  Created by Thulani Mtetwa on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
