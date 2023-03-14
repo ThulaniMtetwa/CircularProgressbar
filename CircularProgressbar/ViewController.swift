c4fe926a10cbd46e181742b5ba48f2c6d54467b0
Thulani Mtetwa <Thulani.Mtetwa@standardbank.co.za>
2023-03-14 13:40:06 +0200

    add progress bar

--- CircularProgressbar/ViewController.swift
+++ CircularProgressbar/ViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var circleOnSceneView: CircularProgressView!{
+        didSet {
+            circleOnSceneView.setProgressColor = UIColor(displayP3Red: 50.0/255.0, green: 168.0/255.0, blue: 82.0/255.0, alpha: 1.0)
+            circleOnSceneView.setTrackColor = UIColor(displayP3Red: 205.0/255.0, green: 247.0/255.0, blue: 212.0/255.0, alpha: 1.0)
+            circleOnSceneView.setProgressWithAnimation(duration: 2.0, value: 0.4)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
